2226ae6764ce7b848c69180e9a2ee9e8467c7e74
felcg <xfelipecg@gmail.com>
2020-07-04 07:30:00 -0300

    Adiciona rota para pagina Parar

--- src/Router.js
+++ src/Router.js
@@ -4,9 +4,11 @@ import { Route, Switch, withRouter } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Jogo from './pages/Jogo/Jogo'
 import Vitoria from './pages/Vitoria/Vitoria'
+import Parar from './pages/Parar/Parar'
 
 export default () => (
   <Switch>
+    <Route exact path="/parar" component={withRouter(Parar)} />
     <Route exact path="/vitoria" component={withRouter(Vitoria)} />
     <Route exact path="/jogo" component={withRouter(Jogo)} />
     <Route exact path="/" component={withRouter(Home)} />
