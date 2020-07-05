2ba3655623b9db7f9acfb6af6cde7d5b3eec9d32
felcg <xfelipecg@gmail.com>
2020-07-05 07:23:50 -0300

    Adiciona rota para ranking

--- src/Router.js
+++ src/Router.js
@@ -5,9 +5,11 @@ import Home from './pages/Home/Home'
 import Jogo from './pages/Jogo/Jogo'
 import Vitoria from './pages/Vitoria/Vitoria'
 import Parar from './pages/Parar/Parar'
+import Ranking from './pages/Ranking/Ranking'
 
 export default () => (
   <Switch>
+    <Route exact path="/ranking" component={withRouter(Ranking)} />
     <Route exact path="/parar" component={withRouter(Parar)} />
     <Route exact path="/vitoria" component={withRouter(Vitoria)} />
     <Route exact path="/jogo" component={withRouter(Jogo)} />
