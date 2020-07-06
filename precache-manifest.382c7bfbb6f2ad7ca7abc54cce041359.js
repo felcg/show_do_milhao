087663141a35e6342470773b829b0e3641567fe1
felcg <xfelipecg@gmail.com>
2020-07-06 03:34:08 -0300

    Updates

---
+++ precache-manifest.382c7bfbb6f2ad7ca7abc54cce041359.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970aeb361d15612cc919b1ea9b658261",
+    "url": "/show_do_milhao/index.html"
+  },
+  {
+    "revision": "fcd0e08d0d9a51f989a0",
+    "url": "/show_do_milhao/static/css/main.6db47593.chunk.css"
+  },
+  {
+    "revision": "b420f77bcef7ebdc8480",
+    "url": "/show_do_milhao/static/js/2.ca449b3b.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/show_do_milhao/static/js/2.ca449b3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcd0e08d0d9a51f989a0",
+    "url": "/show_do_milhao/static/js/main.4f8ffb21.chunk.js"
+  },
+  {
+    "revision": "02efc72cedece8531389",
+    "url": "/show_do_milhao/static/js/runtime-main.76c8427f.js"
+  },
+  {
+    "revision": "957a17ecdeb5937e9fd11d52102d3362",
+    "url": "/show_do_milhao/static/media/HEART-1.957a17ec.svg"
+  },
+  {
+    "revision": "f031b4b1a3f97c1d1264129413fd9f0d",
+    "url": "/show_do_milhao/static/media/HEART-13-KING.f031b4b1.svg"
+  },
+  {
+    "revision": "980aeeb22d0495edc3985810afdde6dd",
+    "url": "/show_do_milhao/static/media/HEART-2.980aeeb2.svg"
+  },
+  {
+    "revision": "ba9242971d6d4187e9b943b85368d24c",
+    "url": "/show_do_milhao/static/media/HEART-3.ba924297.svg"
+  },
+  {
+    "revision": "19c6c6bc70e5806b4fe932f6becfce9e",
+    "url": "/show_do_milhao/static/media/cartas black.19c6c6bc.svg"
+  },
+  {
+    "revision": "dff7f894cf0af1775399becf7927a0fa",
+    "url": "/show_do_milhao/static/media/convidado black.dff7f894.svg"
+  },
+  {
+    "revision": "cc422fe9f4aecf2b37b71fca364ddbcc",
+    "url": "/show_do_milhao/static/media/embarrassed.cc422fe9.svg"
+  },
+  {
+    "revision": "9e446e87bcca7c8ae2fa0c472d0b73fc",
+    "url": "/show_do_milhao/static/media/happy.9e446e87.svg"
+  },
+  {
+    "revision": "0181037e9d888cb48e75d342632f2a75",
+    "url": "/show_do_milhao/static/media/logo.0181037e.png"
+  },
+  {
+    "revision": "ef0abe78fe14c373f253753a88928d67",
+    "url": "/show_do_milhao/static/media/parar.ef0abe78.jpg"
+  },
+  {
+    "revision": "4cfc11b407b419f5db347405af307440",
+    "url": "/show_do_milhao/static/media/placas black.4cfc11b4.svg"
+  },
+  {
+    "revision": "c2b9314f498fe9ff3a178ac186fe0527",
+    "url": "/show_do_milhao/static/media/skip black.c2b9314f.svg"
+  },
+  {
+    "revision": "7ef64d4a043ab578dc32325b20150fbe",
+    "url": "/show_do_milhao/static/media/stop black.7ef64d4a.svg"
+  },
+  {
+    "revision": "d554d5acc9749d174aa65b51976de52f",
+    "url": "/show_do_milhao/static/media/trophy.d554d5ac.svg"
+  },
+  {
+    "revision": "d97cf27f0ba99096a2087c2a16c21bb8",
+    "url": "/show_do_milhao/static/media/vitoria.d97cf27f.jpg"
+  }
+]);
