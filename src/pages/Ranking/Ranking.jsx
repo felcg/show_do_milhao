c9de530a4328f9a9baf35979340d7d4b4dd7add7
felcg <xfelipecg@gmail.com>
2020-07-06 00:50:18 -0300

    Adiciona opcao de resetar ranking na tela de ranking

--- src/pages/Ranking/Ranking.jsx
+++ src/pages/Ranking/Ranking.jsx
@@ -1,14 +1,23 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable react/no-array-index-key */
-import React from 'react'
+import React, { useState } from 'react'
+import { Modal } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import './Ranking.scss'
 import trofeu from '../../assets/trophy.svg'
 
 const Ranking = () => {
+  const [resetVisivel, setResetVisivel] = useState(false)
   const ranking = JSON.parse(localStorage.getItem('ranking'))
   let top10 = []
   if (ranking) {
     top10 = ranking.sort((a, b) => b.valor - a.valor).slice(0, 10)
+  }
+
+  const handleReset = () => {
+    localStorage.clear()
+    setResetVisivel(false)
   }
 
   return (
@@ -17,7 +26,10 @@ const Ranking = () => {
         ? (
           <div className="ranking">
 
-            <h1 className="ranking__titulo">Top 10 Jogadores</h1>
+            <div className="ranking__bloco">
+              <h1 className="ranking__titulo">Top 10</h1>
+              <button className="botao botao__voltar" onClick={() => setResetVisivel(true)}>Reiniciar ranking</button>
+            </div>
             {top10.map((jogador, index) => (
               index + 1 === 1
                 ? (
@@ -36,17 +48,27 @@ const Ranking = () => {
                 )
             ))}
 
-            <Link to="/" className="ranking__voltar"><button className="botao__jogar">Voltar</button></Link>
+            <Link to="/" className="ranking__voltar"><button className="botao">Voltar</button></Link>
+
           </div>
+
         )
         : (
           <div className="ranking--vazio">
             <div>
               <p>Ainda não temos um ranking, que tal começarmos a jogar?</p>
-              <Link to="/"><button className="botao__jogar">Voltar</button></Link>
+              <Link to="/"><button className="botao">Voltar</button></Link>
             </div>
           </div>
         )}
+
+      <Modal centered show={resetVisivel} backdrop="static" keyboard={false} onHide={() => setResetVisivel(false)}>
+        <div className="confirmacao">
+          <p className="confirmacao__titulo">Tem certeza? <br /> Não poderá voltar atrás.</p>
+          <button className="confirmacao__botao confirmacao__botao--confirmar" onClick={handleReset}>Sim</button>
+          <button className="confirmacao__botao confirmacao__botao--cancelar" onClick={() => setResetVisivel(false)}>Não</button>
+        </div>
+      </Modal>
     </>
   )
 }
